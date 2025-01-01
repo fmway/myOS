07191e81895c82e96ddb6bbf7b9e3fa428440923
fmway <fm18lv@gmail.com>
2025-01-02 03:54:50 +0700

    chore(cachix): add some cachix url

---
+++ cachix/niv.nix
@@ -0,0 +1,13 @@
+
+{
+  nix = {
+    settings = {
+      substituters = [
+        "https://niv.cachix.org"
+      ];
+      trusted-public-keys = [
+        "niv.cachix.org-1:X32PCg2e/zAm3/uD1ScqW2z/K0LtDyNV7RdaxIuLgQM="
+      ];
+    };
+  };
+}
