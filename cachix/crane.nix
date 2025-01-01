704a44b4acd8b5a8ed82a6aed9ad0d1f793542b2
fmway <fm18lv@gmail.com>
2025-01-02 03:54:50 +0700

    chore(cachix): add some cachix url

---
+++ cachix/crane.nix
@@ -0,0 +1,13 @@
+
+{
+  nix = {
+    settings = {
+      substituters = [
+        "https://crane.cachix.org"
+      ];
+      trusted-public-keys = [
+        "crane.cachix.org-1:8Scfpmn9w+hGdXH/Q9tTLiYAE/2dnJYRJP7kl80GuRk="
+      ];
+    };
+  };
+}
