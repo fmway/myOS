575689e5bc705069820e89b2ad1015cc2cd2f715
fmway <fm18lv@gmail.com>
2025-01-04 13:34:08 +0700

    chore(home-manager): zed editor

---
+++ home-manager/default/programs/zed-editor.nix
@@ -0,0 +1,43 @@
+{ pkgs, ... }:
+{
+  extraPackages = with pkgs; [
+    rust-analyzer
+    gopls
+  ];
+
+  userSettings = {
+    ui_font_size = 16;
+    buffer_font_size = 16;
+    vim_mode = true;
+    theme = "Catppuccin Macchiato";
+    auto_update = false;
+    relative_line_numbers = true;
+
+    lsp = {
+      rust-analyzer.initialization_options = {
+        snippets = {
+          "Arc::new" = {
+            "postfix" = "arc";
+            "body" = ["Arc::new(\${receiver})"];
+            "requires" = "std::sync::Arc";
+            "scope" = "expr";
+          };
+          "Some" = {
+            "postfix" = "some";
+            "body" = ["Some(\${receiver})"];
+            "scope" = "expr";
+          };
+          "Ok" = {
+            "postfix" = "ok";
+            "body" = ["Ok(\${receiver})"];
+            "scope" = "expr";
+          };
+        };
+        rust.analyzerTargetDir = true;
+        check.command = "clippy";
+      };
+    };
+  };
+
+  extensions = [ "nix" ];
+}
