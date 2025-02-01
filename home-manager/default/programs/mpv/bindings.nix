4e569a6b807f5e60ed7ea13bcac59a19852e407b
fmway <fm18lv@gmail.com>
2025-02-01 09:44:43 +0700

    chore(home-manager): add mpv.config

---
+++ home-manager/default/programs/mpv/bindings.nix
@@ -0,0 +1,12 @@
+{
+  # zoom video
+  "Alt+-" = "add video-zoom -0.25";
+  "Alt+=" = "add video-zoom 0.25";
+
+  # Rotate video
+  "ctrl+left" = ''cycle-values video-rotate "90" "180" "270" "0'';
+
+  # mirroring video
+  "ctrl+l"= "vf oggle hflip";
+  "ctrl+v"= "vf toggle vflip";
+}
--- home-manager/default/programs/mpv/default.nix
+++ home-manager/default/programs/mpv/default.nix
@@ -1,6 +1,40 @@
-{ config, ... }:
+{ config, pkgs, ... }:
 {
   enable = ! config.data.isMinimal or false;
-  config = {};
+  config = {
+    volume-max = "300";
+    hwdec = "auto-safe";
+    vo = "gpu";
+    profile = "gpu-hq";
+    gpu-context = "wayland";
+  };
   bindings = {};
+
+  scripts = (with pkgs.mpvScripts;
+  [
+    youtube-upnext
+    webtorrent-mpv-hook
+    visualizer
+    sponsorblock
+    # seekTo
+    reload
+    quality-menu
+    quack
+    mpv-playlistmanager
+    autosubsync-mpv
+    # mpv-osc-modern
+    mpv-cheatsheet
+    mpris
+    # modernx
+    memo
+    thumbfast
+    # manga-reader
+    inhibit-gnome
+    evafast
+    uosc
+  ]) ++ (with pkgs.fmpkgs.mpv-scripts; [
+    multiloop
+    menu
+    bookmarker
+  ]);
 }
