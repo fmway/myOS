73fa6934f324419ed13fc1ec3b4611a2a17fa67f
fmway <fm18lv@gmail.com>
2025-02-09 19:04:49 +0700

    fix(home-manager/modules): handle when source doesn't found and add some xdg file

--- home-manager/modules/files.nix
+++ home-manager/modules/files.nix
@@ -1,4 +1,4 @@
-{ lib, config, ... }: let
+{ lib, config, pkgs, ... }: let
   cfg = config.home.file;
   home = config.home.homeDirectory;
   filtered =  lib.filter (x: !cfg.${x}.symlink) (lib.attrNames cfg);
@@ -6,13 +6,15 @@
 
   script = lib.concatStringsSep "\n" (map (x: let
     self = cfg.${x};
+    tsource = pkgs.writeText x self.text;
+    source = self.source or tsource;
   in  /* sh */ ''
     target=${home}/${self.target}
     ${if self.recursive then "rm -rf" else "unlink"} $target
-    if test -f ${self.source}; then
-      cat ${self.source} > $target
+    if test -f ${source}; then
+      cat ${source} > $target
     else
-      cp -rf ${self.source} $target
+      cp -rf ${source} $target
     fi
     ${lib.optionalString (! isNull self.executable && self.executable) "chmod -R +x $target"}
   '') filtered);
@@ -25,7 +27,7 @@ in {
     };
   in {
     home.file = optionType;
-    xdg.configFile = optionType;
+    xdg = lib.genAttrs [ "configFile" "dataFile" "stateFile" ] (_: optionType);
   };
 
   config = lib.mkIf isEnabled {
