ad84168b978393cd77d4b30f8b50dc14cda7079e
fmway <fm18lv@gmail.com>
2025-03-30 02:00:54 +0700

    chore(systems): ...

--- systems/networking/default.nix
+++ systems/networking/default.nix
@@ -83,20 +83,20 @@
   # resolvconf.enable = false;
 
   # handling Wireguard to firewall
-  # firewall = {
-  #   checkReversePath = "loose";
-  #   # if packets are still dropped, they will show up in dmesg
-  #   logReversePathDrops = true;
-  #   # up
-  #   extraCommands = ''
-  #     ip46tables -t mangle -I nixos-fw-rpfilter -p udp -m udp --sport 51820 -j RETURN
-  #     ip46tables -t mangle -I nixos-fw-rpfilter -p udp -m udp --dport 51820 -j RETURN
-  #   '';
-  #
-  #   # down
-  #   extraStopCommands = ''
-  #     ip46tables -t mangle -D nixos-fw-rpfilter -p udp -m udp --sport 51820 -j RETURN || true
-  #     ip46tables -t mangle -D nixos-fw-rpfilter -p udp -m udp --dport 51820 -j RETURN || true
-  #   '';
-  # };
+  firewall = {
+    checkReversePath = "loose";
+    # if packets are still dropped, they will show up in dmesg
+    logReversePathDrops = true;
+    # up
+    extraCommands = ''
+      ip46tables -t mangle -I nixos-fw-rpfilter -p udp -m udp --sport 51820 -j RETURN
+      ip46tables -t mangle -I nixos-fw-rpfilter -p udp -m udp --dport 51820 -j RETURN
+    '';
+
+    # down
+    extraStopCommands = ''
+      ip46tables -t mangle -D nixos-fw-rpfilter -p udp -m udp --sport 51820 -j RETURN || true
+      ip46tables -t mangle -D nixos-fw-rpfilter -p udp -m udp --dport 51820 -j RETURN || true
+    '';
+  };
 }
