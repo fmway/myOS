e241d2ed37de53b895e3ba65ca09ef4396e4c15b
fmway <fm18lv@gmail.com>
2025-05-12 18:29:31 +0700

    chore: add starship settings

--- modules/home-manager/starship.nix
+++ modules/home-manager/starship.nix
@@ -2,6 +2,13 @@
 {
   programs.starship = {
     enable = lib.mkDefault true;
-    settings = {};
+    settings = {
+      add_newline = false;
+      enableTranscience = true;
+      character = {
+        success_symbol = "[➜](bold green)";
+        error_symbol = "[➜](bold red)";
+      };
+    };
   };
 }
