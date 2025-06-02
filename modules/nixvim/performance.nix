f6648c6d80c51a8cee48208352ba4e65d9d6c746
fmway <fm18lv@gmail.com>
2025-06-03 06:22:17 +0700

    feat(modules/nixvim/performance): init

---
+++ modules/nixvim/performance.nix
@@ -0,0 +1,12 @@
+{
+  performance = {
+    byteCompileLua = {
+      enable = true;
+      configs = true;
+      initLua = true;
+      luaLib = true;
+      nvimRuntime = true;
+      plugins = true;
+    };
+  };
+}
