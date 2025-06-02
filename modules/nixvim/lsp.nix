77015cda102d00a65cf05d3c57af9e8897a364c4
fmway <fm18lv@gmail.com>
2025-06-03 06:25:03 +0700

    feat(modules/nixvim/lsp): add tinymist

--- modules/nixvim/lsp.nix
+++ modules/nixvim/lsp.nix
@@ -64,5 +64,31 @@
       __raw = "myNixd()";
       diagnostic.suppress = [ "sema-escaping-with" ];
     };
+
+    tinymist.enable = true;
+    tinymist.cmd = ["tinymist"];
+    tinymist.rootMarkers = [ ".git" ];
+    tinymist.filetypes = [ "typst" ];
+    tinymist.settings = {
+      formatterMode = "typstyle";
+      rootPath.__raw = ''vim.env["TYPST_ROOT"] or nil'';
+    };
+    tinymist.onAttach.function = ''
+      vim.keymap.set("n", "<leader>tp", function()
+        client:exec_cmd({
+          title = "pin",
+          command = "tinymist.pinMain",
+          arguments = { vim.api.nvim_buf_get_name(0) },
+        }, { bufnr = bufnr })
+      end, { desc = "[T]inymist [P]in", noremap = true })
+
+      vim.keymap.set("n", "<leader>tu", function()
+        client:exec_cmd({
+          title = "unpin",
+          command = "tinymist.pinMain",
+          arguments = { vim.v.null },
+        }, { bufnr = bufnr })
+      end, { desc = "[T]inymist [U]npin", noremap = true })
+    '';
   };
 }
