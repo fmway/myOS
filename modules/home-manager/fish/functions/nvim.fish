e3717be1d6ebc3e4f9be5da19373c029a68ff712
fmway <fm18lv@gmail.com>
2025-06-06 14:47:53 +0700

    chore(modules/home-manager/fish): nvim functions with zoxide support

--- modules/home-manager/fish/functions/nvim.fish
+++ modules/home-manager/fish/functions/nvim.fish
@@ -1,5 +1,10 @@
 # @description simple suck for handling folder
-if set -q argv[1] && [ -d "$argv[1]" ]
+if set -q argv[1]
+  if [ -d "$argv[1]" ]
+    ! command -q zoxide || zoxide add $argv
     set argv[1] +"tcd $argv[1]"
+  else if command -q zoxide && set dir (zoxide query "$argv[1]")
+    set argv[1] +"tcd $dir"
+  end
 end
 command nvim $argv
