6db370515fb37c9cb07355a231aa7c9c657b6141
fmway <fm18lv@gmail.com>
2025-06-07 21:23:43 +0700

    feat: drop master branch (very useless), add nixosConfigurations.default

---
+++ hosts/default/configuration.nix
@@ -0,0 +1,26 @@
+{ internal, inputs, config, selfConfig ? config, selfInputs ? inputs, ... }:
+{ inputs, config, lib, pkgs, ... }:
+{
+  imports = with inputs; [
+    ./hardware-configuration.nix
+    selfConfig.flake.nixosModules.default
+    fmpkgs.nixosModules.default
+  ];
+
+  # Enable fwupd for updating firmware
+  services.fwupd.enable = true;
+
+  programs.git.enable = true;
+
+  programs.cloudflared = {
+    enable = true;
+    secretFile = config.age.secrets.cloudflared.path;
+  };
+
+  # Some programs need SUID wrappers, can be configured further or are started in user sessions.
+  programs.mtr.enable = true;
+  programs.gnupg.agent = {
+    enable = true;
+    enableSSHSupport = true;
+  };
+}
