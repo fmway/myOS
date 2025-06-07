103930b603abd26e3ad4a6f13ba6948d651957a5
fmway <fm18lv@gmail.com>
2025-06-07 21:04:59 +0700

    chore(modules/home-manager/fish): add some conditions in nvim function

--- modules/home-manager/fish/functions/nvim.fish
+++ modules/home-manager/fish/functions/nvim.fish
@@ -1,9 +1,13 @@
 # @description simple suck for handling folder
 if set -q argv[1]
+  # if first args is directory
   if [ -d "$argv[1]" ]
     ! command -q zoxide || zoxide add $argv
     set argv[1] +"tcd $argv[1]"
-  else if command -q zoxide && set dir (zoxide query "$argv[1]")
+  else if [ (string split '' -- $argv[1])[1] != "+" ]
+    and ! [ -e "$argv[1]" ]
+    and command -q zoxide
+    and set dir (zoxide query "$argv[1]" 2>/dev/null)
     set argv[1] +"tcd $dir"
   end
 end
